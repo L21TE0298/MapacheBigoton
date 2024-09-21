58994cebe55e45441c12a824e6dc9104645b25a2
Felix <cowboy.spaceboy123@gmail.com>
2024-09-21 00:35:49 -0600

    FrontEnd Componentes, JS y CSS actualizados

--- mapachefront/src/App.js
+++ mapachefront/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Inicio from './Components/Inicio'
+import ConsultarCitas from './Components/ConsultarCitas';
+import Barbero from './Components/NuevoBarbero';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/consultas" element={<ConsultarCitas />} />
+        <Route path="/barbero" element={<Barbero />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ mapachefront/src/Components/NuevoBarbero.js
@@ -0,0 +1,11 @@
+import './css/inicio.css';
+import imagenes from '../imagenes';
+
+function Barbero(){
+    return (
+        <div className="App">
+           
+        </div>
+    );
+}
+export default Barbero;
